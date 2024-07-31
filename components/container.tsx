bd844c773c30119e921eaa6a46aff151507baf66
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2024-07-31 19:51:58 +0700

    fix: import error

--- components/container.tsx
+++ components/container.tsx
@@ -1,22 +1,19 @@
-import resolveConfig from "tailwindcss/resolveConfig";
-import _tailwindConfig from "@/tailwind.config.js";
 import { cn } from "@/lib/utils";
-const tailwindConfig = resolveConfig(_tailwindConfig);
 export default function Container({
   children,
   max,
 }: {
   children: React.ReactNode;
-  max: "sm" | "md" | "lg" | "xl" | "liquid";
+  max?: "sm" | "md" | "lg" | "xl" | "liquid";
 }) {
   return (
     <main className="p-4 md:px-10">
       <div
         className={cn(
           "mx-auto w-full gap-4 grid",
-          "max-w-screen-sm",
+          "max-w-screen-lg",
           max === "md" && "max-w-screen-md",
-          max === "lg" && "max-w-screen-lg",
+          max === "sm" && "max-w-screen-sm",
           max === "xl" && "max-w-screen-xl",
           max === "liquid" && "max-w-full",
         )}
