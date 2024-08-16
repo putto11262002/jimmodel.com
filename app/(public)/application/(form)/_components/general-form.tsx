15731310a0784355d27b6d3f31999885f875876e
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2024-08-16 13:57:06 +0700

    allow month and year select in application form

--- app/(public)/application/(form)/_components/general-form.tsx
+++ app/(public)/application/(form)/_components/general-form.tsx
@@ -42,6 +42,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import DatetimePicker from "@/components/datetime-picker";
 
 const formDataSchema = ApplicationCreateInputSchema.pick({
   name: true,
@@ -125,38 +126,11 @@ export default function GeneralForm() {
               render={({ field }) => (
                 <FormItem className="col-span-1 space-y-2">
                   <FormLabel>Date of Birth</FormLabel>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <FormControl>
-                        <Button
-                          type="button"
-                          className={cn(
-                            "w-full pl-3 text-left font-normal",
-                            !field.value && "text-muted-foreground",
-                          )}
-                          variant={"outline"}
-                        >
-                          {field.value ? ( // format(field.value, "PPP")
-                            dayjs(field.value).format("MMM DD, YYYY")
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                        </Button>
-                      </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value!}
-                        onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
+                  <DatetimePicker
+                    showTime={false}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
                   <FormMessage />
                 </FormItem>
               )}
