829d6ec9633e340f259bb67a3f9a19f67566745c
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2024-08-30 14:40:48 +0700

    refine landing page hero section

--- app/(public)/_components/hero-section.tsx
+++ app/(public)/_components/hero-section.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { webAssetUseCase } from "@/lib/usecases";
 import ImageSlider from "./image-slider";
 import webConfig from "@/config/web";
@@ -12,11 +10,14 @@ export default async function HeroSection() {
   });
   return (
     <section className="relative h-[calc(100vh-theme(spacing.20))] ">
-      <div className="absolute px-4 md:px-0 w-full h-full inset-0 z-20 bg-gradient-to-b from-background to-background/0">
-        <h1 className="text-4xl font-bold text-center mt-20">
+      <div className="absolute px-4 flex md:block md:px-0 w-full h-full inset-0 z-20 bg-gradient-to-b from-background/40 via-background/80 to-background/40 md:bg-gradient-to-b md:from-background md:to-background/0">
+        <h1 className="hidden md:block text-2xl my-auto md:my-0 md:text-4xl font-bold text-center md:mt-20">
           {webConfig.heroTitle}
         </h1>
-        <h2 className="pt-2 text-sm max-w-lg w-full mx-auto text-center text-muted-foreground">
+        <h1 className="block md:hidden w-full text-4xl my-auto font-bold text-center md:mt-20">
+          J.I.M.
+        </h1>
+        <h2 className="hidden md:block pt-2 text-sm max-w-lg w-full mx-auto text-center text-muted-foreground">
           {webConfig.heroSubTitle}
         </h2>
         {/* <div className="w-full flex pt-6 justify-center"> */}
