c5c783eb83d701c075337abc69e11878764c8cac
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2025-11-05 22:24:50 +0700

    feat: export model script

--- scripts/export_data.ts
+++ scripts/export_data.ts
@@ -1,5 +1,5 @@
 import { fileUseCase, modelUseCase } from "@/config";
-import { Model } from "@/lib/domains";
+import { Model, ModelImage } from "@/lib/domains";
 import fs from "fs/promises";
 
 // Grab all the models from the database
@@ -8,7 +8,7 @@ let hasMore = true;
 
 const outputDir = "./exports/models";
 
-const modelOutput: (Model & { modelProfileImagePath: string | null })[] = [];
+const modelOutput: (Model & { images: ModelImage[] })[] = [];
 
 async function main() {
   await fs.mkdir(outputDir, { recursive: true });
@@ -24,36 +24,36 @@ async function main() {
     for (const model of models.data) {
       try {
         // save model images
-        const modelAssetsDir = `${outputDir}/${model.id}/assets`;
-        await fs.mkdir(`${outputDir}/${model.id}`, { recursive: true });
-        await fs.mkdir(modelAssetsDir, { recursive: true });
+        // const modelAssetsDir = `${outputDir}/${model.id}/assets`;
+        // await fs.mkdir(`${outputDir}/${model.id}`, { recursive: true });
+        // await fs.mkdir(modelAssetsDir, { recursive: true });
+        //
+        // const images = await modelUseCase.getModelImages(model.id);
+        // for (const image of images) {
+        //   const imageBlob = await fileUseCase.download(image.fileId);
+        //   console.log(
+        //     "Saving image for model:",
+        //     model.id,
+        //     "image:",
+        //     image.fileId,
+        //   );
+        //   await fs.writeFile(
+        //     `${modelAssetsDir}/${image.fileId}-${image.type}.jpeg`,
+        //     Buffer.from(await imageBlob.arrayBuffer()),
+        //   );
+        //   console.log(
+        //     "Saved image for model:",
+        //     model.id,
+        //     "image:",
+        //     image.fileId,
+        //   );
+        // }
 
         const images = await modelUseCase.getModelImages(model.id);
-        for (const image of images) {
-          const imageBlob = await fileUseCase.download(image.fileId);
-          console.log(
-            "Saving image for model:",
-            model.id,
-            "image:",
-            image.fileId,
-          );
-          await fs.writeFile(
-            `${modelAssetsDir}/${image.fileId}-${image.type}.jpeg`,
-            Buffer.from(await imageBlob.arrayBuffer()),
-          );
-          console.log(
-            "Saved image for model:",
-            model.id,
-            "image:",
-            image.fileId,
-          );
-        }
 
         modelOutput.push({
           ...model,
-          modelProfileImagePath: model.profileImageId
-            ? `${modelAssetsDir}/profile-image.jpeg`
-            : null,
+          images,
         });
         console.log("Exported model:", model.id);
       } catch (error) {
